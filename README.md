fcdc28ec4dce07b71c715dd29863b68bd4aa5e5b
ruqoyah <rukayatodukoya123@gmail.com>
2018-02-08 00:26:17 +0100

    Implement learning plan application

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # SingleApplication
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.6.7.
+An Angular single page application.
 
 ## Development server
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Learning Plan';
+  newStack = '';
+  newStartDate= '';
+  newCompletionDate= ''
+  plans = [];
+
+  ngOnInit() {
+    this.plans = JSON.parse(localStorage.getItem('plans'));
+  }
+
+  // To add new learning map
+  addPlan(event){
+
+    let planObj = {
+      stack : this.newStack,
+      startDate: this.newStartDate,
+      completionDate: this.newCompletionDate
+    }
+    this.plans.push(planObj);
+    localStorage.setItem('plans', JSON.stringify(this.plans))
+    this.newStack = '';
+    this.newStartDate = '';
+    this.newCompletionDate = '';
+  }
+
 }
